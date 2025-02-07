9ca6abbf48ac69312146566f3514e68f006dd30e
kalviumcommunity <sibishree2002@gmail.com>
2025-02-07 12:16:16 +0530

    connected to the mongodb atlas

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const { resolve } = require('path');
-
+const mongoose = require('mongoose');
 const app = express();
 const port = 3010;
-
+require('dotenv').config();
 app.use(express.static('static'));
+
+const dbUrl=process.env.db_url;
+mongoose.connect(dbUrl)
+.then((data)=>{
+  console.log(`Connected to the mongo Atlas ${data.connection.host}`); 
+})
+.catch((err)=>{
+  console.log("Connection Failed.",err) 
+  process.exit(1); 
+})
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
